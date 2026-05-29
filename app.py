bd030f7d69c403a05e302b777e4878ac783a4f4b
gyr0byte <gqurav69@gmail.com>
2026-05-29 21:06:50 +0545

    Fix formatting and update column method in Streamlit app; enhance fetch_stats function for better handling of empty dataframes

--- app.py
+++ app.py
@@ -10,19 +10,20 @@ if uploaded_file is not None:
     data = bytes_data.decode("utf-8")
     df = preprocessor.preprocess(data)
     st.dataframe(df)
-    
-    #fetch unique users
+
+    # fetch unique users
     user_list = df['user'].unique().tolist()
     if 'group_notification' in user_list:
         user_list.remove('group_notification')
     user_list.sort()
     user_list.insert(0, "Overall")
-    
-    
-    selected_user = st.sidebar.selectbox("Show analysis with respect to", user_list)
-    
+
+    selected_user = st.sidebar.selectbox(
+        "Show analysis with respect to", user_list)
+
     if st.sidebar.button("Show Analysis"):
-        col1, col2, col3, col4 = st.beta_columns(4)
+        col1, col2, col3, col4 = st.columns(4)
         num_messages = helper.fetch_stats(selected_user, df)
         with col1:
             st.header("Total Messages")
+            st.title(num_messages)
--- helper.py
+++ helper.py
@@ -1,3 +1,7 @@
 def fetch_stats(selected_user, df):
+    if df is None or df.empty:
+        return 0
     if selected_user == "Overall":
         return df.shape[0]
+    else:
+        return df[df["user"] == selected_user].shape[0]
