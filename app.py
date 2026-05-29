6e9521deccb8a43a7bc389b4fea33735bae19469
gyr0byte <gqurav69@gmail.com>
2026-05-29 18:16:50 +0545

    Fix user list processing to conditionally remove 'group_notification' and ensure proper analysis display in Streamlit app

--- app.py
+++ app.py
@@ -12,9 +12,16 @@ if uploaded_file is not None:
     
     #fetch unique users
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
+    if 'group_notification' in user_list:
+        user_list.remove('group_notification')
     user_list.sort()
     user_list.insert(0, "Overall")
     
     
     st.sidebar.selectbox("Show analysis with respect to", user_list)
+    
+    if st.sidebar.button("Show Analysis"):
+        col1, col2, col3, col4 = st.beta_columns(4)
+
+        with col1:
+            st.header("Total Messages")
