85b71c4049120c0a69768d6bd1af7ba5e2a8a7c0
gyr0byte <gqurav69@gmail.com>
2026-05-30 18:51:14 +0545

    Refactor fetch_stats function to handle empty dataframes and streamline word count calculation

--- helper.py
+++ helper.py
@@ -1,19 +1,11 @@
 def fetch_stats(selected_user, df):
-    if df is None or df.empty:
-        return 0
-    if selected_user == "Overall":
-        # 1. Fetch number of messages
-        num_messages = df.shape[0]
-        #2. number of words
-        words = 0
-        for message in df['message']:
-            message_words = message.split()
-            words += len(message_words)
-        return num_messages, words
-    else:
-        num_messages = df[df["user"] == selected_user].shape[0]
-        words = 0
-        for message in df[df["user"] == selected_user]["message"]:
-            message_words = message.split()
-            words += len(message_words)
-        return num_messages, words
+    if selected_user != "Overall":
+        df = df[df['user'] == selected_user]
+
+    num_messages = df.shape[0]
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+    num_words = len(words)
+
+    return num_messages, num_words
