4a0fc1ffb49afa94b4b3a0bf3c69bc8d8d4746bb
gyr0byte <gqurav69@gmail.com>
2026-05-30 18:42:03 +0545

    Refactor fetch_stats function to return both message count and word count for selected user and overall

--- helper.py
+++ helper.py
@@ -2,6 +2,18 @@ def fetch_stats(selected_user, df):
     if df is None or df.empty:
         return 0
     if selected_user == "Overall":
-        return df.shape[0]
+        # 1. Fetch number of messages
+        num_messages = df.shape[0]
+        #2. number of words
+        words = 0
+        for message in df['message']:
+            message_words = message.split()
+            words += len(message_words)
+        return num_messages, words
     else:
-        return df[df["user"] == selected_user].shape[0]
+        num_messages = df[df["user"] == selected_user].shape[0]
+        words = 0
+        for message in df[df["user"] == selected_user]["message"]:
+            message_words = message.split()
+            words += len(message_words)
+        return num_messages, words
