ee9c55a7df509039b3e30f9840c8bf98a77a250a
gyr0byte <gqurav69@gmail.com>
2026-05-30 19:31:41 +0545

    Fix subplot creation and ensure x-axis labels are rotated for better readability

--- app.py
+++ app.py
@@ -42,9 +42,10 @@ if uploaded_file is not None:
         if selected_user == 'Overall':
             st.title('Most Busy Users')
             x = helper.most_busy_users(df)
-            fig, ax = plt.subplot()
+            fig, ax = plt.subplots()
             col1, col2 = st.columns(2)
             
             with col1:
                 ax.bar(x.index, x.values)
+                plt.xticks(rotation='vertical')
                 st.pyplot(fig)
