d9e8bad222c8b4cedd31b3f6206dcaeb3ffd7f12
gyr0byte <gqurav69@gmail.com>
2026-05-30 18:55:58 +0545

    Update fetch_stats function to return media message count and adjust analysis display accordingly

--- helper.py
+++ helper.py
@@ -2,10 +2,16 @@ def fetch_stats(selected_user, df):
     if selected_user != "Overall":
         df = df[df['user'] == selected_user]
 
+    # fetch number of messages
     num_messages = df.shape[0]
+    
+    # fetch number of words
     words = []
     for message in df['message']:
         words.extend(message.split())
     num_words = len(words)
+    
+    # fetch number of media messages
+    num_media_messages = df[df['message'] == '<Media omitted>'].shape[0]
 
-    return num_messages, num_words
+    return num_messages, num_words, num_media_messages
