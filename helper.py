06db3181a979329e6e1cead3230715ec91c42096
gyr0byte <gqurav69@gmail.com>
2026-05-31 18:33:54 +0545

    Add newline at end of file in create_wordcloud function

--- helper.py
+++ helper.py
@@ -34,4 +34,6 @@ def create_wordcloud(selected_user, df):
     if selected_user != "Overall":
         df = df[df['user'] == selected_user]
 
-    
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    df_wc = wc.generate(df['message'].str.cat(sep=" "))
+    return df_wc
