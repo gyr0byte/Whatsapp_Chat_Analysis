869e66e065e5bb570f4f61f256e05f0e180abe4e
gyr0byte <gqurav69@gmail.com>
2026-05-31 18:35:56 +0545

    Add word cloud visualization for selected user analysis

--- app.py
+++ app.py
@@ -55,3 +55,7 @@ if uploaded_file is not None:
 
 
         #wordcloud
+        df_wc = helper.create_wordcloud(selected_user,df)
+        fig,ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
